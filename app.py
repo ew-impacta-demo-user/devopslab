ab1c7cce45a16668b533aef207563d8bf1629bc0
ew-impacta-demo-user <94020764+ew-impacta-demo-user@users.noreply.github.com>
2022-05-21 09:57:48 -0300

    Fix CSRF

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)
+
 @app.route("/")
 def pagina_inicial():
-    return "Obrigado Gabi - Acho que estou entendendo! :D"
+    return "Laboratório Pipeline DevOps"
+
+if __name__ == '__main__':
+    app.run()
+
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Obrigado Gabi - Acho que estou entendendo! :D.")    
+
 
 if __name__ == '__main__':
     app.run()
