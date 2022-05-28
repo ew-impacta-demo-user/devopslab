915dd79f859c6ad12a486775cf7c07ab23cbec92
ew-impacta-demo-user <94020764+ew-impacta-demo-user@users.noreply.github.com>
2022-05-28 11:51:47 -0300

    Bug Added

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+
 csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Novos Testes"
+    return "Laboratório Pipeline DevOps"
+
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+
 
 if __name__ == '__main__':
-    port = os.getenv('PORT')
-    app.run('0.0.0.0', port=port)
+    app.run()
